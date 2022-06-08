78891f100e8167e26aeb9cd8f18036a281906763
Dernbu <63487502+Dernbu@users.noreply.github.com>
2022-06-09 01:24:43 +0800

    Create merge-two-sorted-lists.py

---
+++ leetcode/src/merge-two-sorted-lists.py
@@ -0,0 +1,62 @@
+# https://leetcode.com/problems/merge-two-sorted-lists/
+"""
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists in a one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+ 
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+ 
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+"""
+
+# Solution:
+# Have two pointers at root and tail of the linked list, and keep adding nodes to a dummy node.
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        root = ListNode()
+        tail = root
+
+        while True:
+            if list1 is None and list2 is None:
+                break
+            elif list1 is None:
+                tail.next = ListNode(list2.val)
+                list2 = list2.next
+            elif list2 is None or list1.val < list2.val:
+                tail.next = ListNode(list1.val)
+                list1 = list1.next
+            else:
+                tail.next = ListNode(list2.val)
+                list2 = list2.next
+            
+            tail = tail.next
+            
+        return root.next
+        
