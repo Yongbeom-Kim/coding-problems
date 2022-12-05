79a83689cfc71708150b678025372a3225664bf3
Dernbu <63487502+Dernbu@users.noreply.github.com>
2022-12-05 10:07:36 +0800

    aoc day 4

--- adventofcode/2022/day_4/part_2.cpp
+++ adventofcode/2022/day_4/part_2.cpp
@@ -18,7 +18,7 @@ int main() {
     int f1,f2,s1,s2;
     int contained = 0;
     while(fscanf(fin, "%d-%d,%d-%d",&f1,&f2,&s1,&s2) == 4) {
-        contained += (f1 <= s1 && f2 >= s2) || (s1 <= f1 && s2 >= f2);
+        contained += (f1 <= s2 && f1 >= s1) || (f2 <= s2 && f2 >= s1) || (s1 <= f2 && s1 >= f1) || (s2 <= f2 && s2 >= f1);
     }
 
     printf("%d", contained);
