e34500af01c7c17e45cbaf0fb4f7dd4366ec20a0
Dernbu <63487502+Dernbu@users.noreply.github.com>
2023-01-07 18:28:46 +0800

    leetcode kclosestpoints python quickselect solution

--- leetcode/src/k-closest-points-to-origin.cpp
+++ leetcode/src/k-closest-points-to-origin.cpp
@@ -16,3 +16,35 @@ public:
         return result;
     }
 };
+
+/*
+    Solution 2: Heap
+*/
+#include <algorithm>
+#include <iterator>
+#include <tuple>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+        auto cmp = [](tuple<double, vector<int>> t1, tuple<double, vector<int>> t2) {
+                return get<0>(t1) > get<0>(t2);
+            };
+        priority_queue<tuple<double, vector<int>>, vector<tuple<double, vector<int>>>, decltype(cmp)> minq(cmp);
+
+        for (vector<int> point: points) {
+            minq.push(make_tuple(point[0]*point[0]+point[1]*point[1], point));
+        }
+
+        vector<vector<int>> result;
+
+        for (int i = 0; i < k; i ++) {
+            result.push_back(get<1>(minq.top()));
+            minq.pop();
+        }
+
+        return result;
+    }
+};
--- leetcode/src/k-closest-points-to-origin.py
+++ leetcode/src/k-closest-points-to-origin.py
@@ -11,3 +11,60 @@ class Solution:
         points = sorted(points, key=keyFunction)
 
         return points[0:k]
+
+
+""" 
+    Solution 2: priority q
+"""
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        h = []
+        for point in points:
+            heappush(h, ((point[0]*point[0]+point[1]*point[1]), point))
+        
+        return [k[1] for k in nsmallest(k, h)]
+
+
+"""
+    Solution 3: quickselect
+"""
+import random
+class Solution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        lo = 0
+        hi = len(points)
+
+        while(True):
+            left, right = self.partition(points, lo, hi)
+            if (left <= k-1 and right >= k-1):
+                return points[:k]
+            elif (left > k-1):
+                hi = left
+            else:
+                lo = right+1
+                
+    
+    def partition(self, points: [List[List[int]]], start, end):
+        pivot = random.randrange(start,end)
+        points[pivot], points[start] = points[start], points[pivot]
+
+        key = self.getDistance(points[start])
+        lt = i = start + 1
+        gt = end - 1
+        while (i <= gt):
+            dist = self.getDistance(points[i])
+            if (dist < key):
+                points[lt], points[i] = points[i], points[lt]
+                i += 1
+                lt += 1
+            elif (dist == key):
+                i += 1
+            else:
+                points[i], points[gt] = points[gt], points[i]
+                gt -= 1
+            # print(str(points) + "|" + str(lt)  + "|" + str(i)  + "|" + str(gt) )
+        points[start], points[lt-1] = points[lt-1], points[start]
+        return lt-1, i-1;
+    
+    def getDistance(self, point: List[int]):
+        return point[0]*point[0]+point[1]*point[1]
